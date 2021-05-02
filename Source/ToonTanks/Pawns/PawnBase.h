a0285e6125f017bd5ff52d6eec6fd79b41902e73
Mike Hill <bushballmarine@gmail.com>
2021-05-02 11:52:07 -0700

    Pawn Setup
    Completed Initial setup on both pawn classes, adding meshes and locating the spawn point for projectiles

--- Source/ToonTanks/Pawns/PawnBase.h
+++ Source/ToonTanks/Pawns/PawnBase.h
@@ -14,15 +14,15 @@ class TOONTANKS_API APawnBase : public APawn
 	GENERATED_BODY()
 
 private:
-	UPROPERTY()
-	UCapsuleComponent* CapsuleComp;
-	UPROPERTY()	
-	UStaticMeshComponent* BaseMesh;
-	UPROPERTY()
-	UStaticMeshComponent* TurretMesh;
-	UPROPERTY()
-	USceneComponent* ProjectileSpawnPoint;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UCapsuleComponent* CapsuleComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* BaseMesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* TurretMesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	USceneComponent* ProjectileSpawnPoint;
 
 public:
 	// Sets default values for this pawn's properties
